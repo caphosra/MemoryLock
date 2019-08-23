fe40a44e9a6e6c5548236f08bb94ae3def481030
Akihisa Yagi <capra314cabra@gmail.com>
2019-08-23 18:22:08 +0900

    Implement non generic classes

--- src/Interfaces/IMemoryAllocater.cs
+++ src/Interfaces/IMemoryAllocater.cs
@@ -1,7 +1,14 @@
+using System;
+
 namespace CapraLib.MemoryLock
 {
     public interface IMemoryAllocater<T> where T : unmanaged
     {
         void SetResult(out T managed);
+    }
+
+    public interface IMemoryAllocater
+    {
+        void SetResult(out Span<byte> bytes);
     }
 }
